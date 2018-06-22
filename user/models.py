9cb1ade770a1ab0f7e87ae8576e69b6946b25106
ashishbansalmbm <40148815+ashishbansalmbm@users.noreply.github.com>
2018-06-22 16:42:09 +0530

    Update models.py

--- user/models.py
+++ user/models.py
@@ -1,62 +1,26 @@
 from django.db import models
-from enum import Enum
+from enumerations.enum import UserType, BloodGroup, Gender, Category
+from django.urls import reverse
 
 
 # Create your models here.
 # User Table
-# enum for User_type
-class UserType(Enum):
-    S = "Student"
-    F = "Faculty"
-    U = "User"
-    A = "Alumni"
-
-
-# enum for blood group
-class BloodGroup(Enum):
-    OM = "O-Minus"
-    OP = "O-Positive"
-    AM = "A-Minus"
-    AP = "A-Positive"
-    BM = "B-Minus"
-    ABM = "AB-Minus"
-    ABP = "AB-Positive"
-    BP = "B-Positive"
-
-
-
-class Gender(Enum):
-    M = "Male"
-    F = "Female"
-    OT = "Other"
-
-
-class Category(Enum):
-    GEN = "General"
-    OBC = "Other Backward Class"
-    SC = "Scheduled Caste"
-    ST = "Scheduled Tribe"
-
 
 class User(models.Model):
     user_name = models.CharField(unique=True, max_length=30)
     password = models.CharField(max_length=256)
     name = models.CharField(max_length=20)
     date_of_birth = models.DateField()
-    gender = [(tag, tag.value) for tag in Gender]
+    gender = models.CharField(max_length=10, choices=[(tag.name, tag.value) for tag in Gender])
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=10)
     state = models.CharField(max_length=10)
     contact = models.CharField(max_length=12)
     email = models.EmailField()
     photo = models.ImageField()
-    category = [(tag, tag.value) for tag in Category]
-    blood_group = [(tag, tag.value) for tag in BloodGroup]
-    type = [(tag, tag.value) for tag in UserType]
+    category = models.CharField(max_length=10, choices=[(tag.name, tag.value) for tag in Category])
+    blood_group = models.CharField(max_length=10, choices=[(tag.name, tag.value) for tag in BloodGroup])
+    type = models.CharField(max_length=10, choices=[(tag.name, tag.value) for tag in UserType])
 
-
-
-
-
-
-
+    def get_absolute_url(self):
+        return reverse('user:user_list')
