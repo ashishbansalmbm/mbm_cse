74b1c72d31ca82f8ae669bd69e843c92dfbeae6f
ashishbansalmbm <40148815+ashishbansalmbm@users.noreply.github.com>
2018-06-25 11:37:04 +0530

    Update views.py

--- user/views.py
+++ user/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import View
 from .models import User
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 class UserList(generic.ListView):
-    template_name = 'user/User_list.html'
+    template_name = 'user/user_list.html'
     context_object_name = 'user_list'
 
     def get_queryset(self):
@@ -14,12 +18,25 @@ class UserList(generic.ListView):
 
 class UserProfile(generic.DetailView):
     model = User
-    template_name = 'user/User_details.html'
+    template_name = 'user/user_details.html'
     context_object_name = 'user_list'
 
 
 class UserCreate(CreateView):
     model = User
-    fields =['User_id', 'Password', 'Name', 'Date_of_birth', 'Sex',
-             'Address', 'City', 'State', 'Contact', 'Email', 'Photo', 'Category', 'Blood_group',
-         ]
+    fields = ['user_name', 'password', 'name', 'date_of_birth', 'gender',
+              'address', 'city', 'state', 'contact', 'email', 'photo', 'category', 'blood_group', 'type']
+
+
+class UserUpdate(UpdateView):
+    model = User
+    fields = ['user_name', 'password', 'name', 'date_of_birth', 'gender',
+              'address', 'city', 'state', 'contact', 'email', 'photo', 'category', 'blood_group', 'type']
+
+
+class UserDelete(DeleteView):
+    model = User
+    success_url = reverse_lazy('user:user_list')
+
+
+
